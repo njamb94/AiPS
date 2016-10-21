e871fb342af97c869d807d01ff484f2d5011710f
njamb94 <>
2016-10-21 13:19:34 +0200

    Prvi commit.

---
+++ src/netbeansproj/NetBeansProj.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeansproj;
+
+/**
+ *
+ * @author student
+ */
+public class NetBeansProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 0;
+    }
+}
